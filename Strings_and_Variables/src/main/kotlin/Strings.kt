15cf5509bb64bc1628fe3b70f95d44444883f9be
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-07-12 10:48:13 -0300

    19. Strings

---
+++ Strings_and_Variables/src/main/kotlin/Strings.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    /**
+     * If a string contains double quotes, they need to be escaped
+     * A character is escaped with a backslash \
+     */
+    println("The answer is \"yes\" so you can proceed")
+
+    /**
+     * If a string contains a backslash, it need to be escaped
+     * a double backslash in a string is interpreted as a sigle backslash
+     */
+    println("I like to put a \\ in my code")
+}
