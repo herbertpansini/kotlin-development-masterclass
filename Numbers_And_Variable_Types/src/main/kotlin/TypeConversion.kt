83b12d2c03d05148cbe14682fae617b7e2c2f552
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-14 17:18:16 -0300

    51. Type Conversions

---
+++ Numbers_And_Variable_Types/src/main/kotlin/TypeConversion.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    val cats = 3
+    val longCats = cats.toLong()
+    println(cats::class.java)
+    println(longCats::class.java)
+    println(longCats)
+
+    val people = 7500000000
+    val fewPeople = people.toInt()
+    println(people::class.java)
+    println(fewPeople::class.java)
+    println(fewPeople)
+
+    val dogs = 5
+    val byteDogs = dogs.toByte()
+    println(byteDogs::class.java)
+    val floatDogs = dogs.toFloat()
+    println(floatDogs::class.java)
+}
