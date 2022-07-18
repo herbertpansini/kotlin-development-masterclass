2e456e5e0dbd9cb78c5e0cf3317e100369d9f02f
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-18 11:06:21 -0300

    Operators

---
+++ Operators_And_Booleans/src/main/kotlin/AugmentedAssignment.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var bikes = 5
+    bikes + 1
+    println(bikes)
+
+    bikes += 3
+    println(bikes)
+
+    bikes -= 4
+    println(bikes)
+
+    bikes *= 2
+    println(bikes)
+
+    bikes /= 2
+    println(bikes)
+
+    bikes = 5
+    bikes %= 2
+    println(bikes)
+}
