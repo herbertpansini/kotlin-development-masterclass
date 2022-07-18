b1a235fde8f7683b8138adae53fe7be3f0a76941
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-18 11:15:54 -0300

    66. Logical Operators

---
+++ Operators_And_Booleans/src/main/kotlin/LogicalOperations.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val isDay = true
+    val isEvening = true
+    println(isDay && isEvening)
+
+    println(false || false)
+
+    val isNight = !isDay
+    println(isNight)
+}
