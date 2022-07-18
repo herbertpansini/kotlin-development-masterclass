f667faf15ebf9d741a5d9f9a2a6dd64725cd4e9a
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-18 10:04:15 -0300

    58. Arithmetic Operators

---
+++ Numbers_And_Variable_Types/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Q
+!src/main/kotlin/TypeConversion.kt,0/5/050b1fdd8ef78e6174b2e84dcc7b53d330e8c6b5
---
+++ Operators_And_Booleans/src/main/kotlin/ArithmeticOperators.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println(5 + 2)
+    println("Hi " + "Jhon")
+    println(5 - 2)
+
+    println(5 * 2)
+    println(5 / 2)
+    println(5f / 2f)
+    println(10 % 5)
+
+    var cats = 5
+    cats++
+    println("A cat passed away. I now have $cats cats")
+
+    var dogs = 2
+    println("I have ${dogs} dogs")
+}
