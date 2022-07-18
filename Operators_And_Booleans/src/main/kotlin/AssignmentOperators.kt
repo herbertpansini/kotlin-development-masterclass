8d6d783a357ef2d785009eefcd305a13663027d8
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-18 10:21:42 -0300

    60. Assignment Operators

---
+++ Operators_And_Booleans/src/main/kotlin/AssignmentOperators.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val dozen = 12
+    val month = dozen
+    println(month)
+
+    val userName = "Lucy"
+    val greeting = "Hello, $userName"
+    println(greeting)
+
+    val products = 3
+    val price = 9.99
+    println("Your total is ${products * price}")
+}
