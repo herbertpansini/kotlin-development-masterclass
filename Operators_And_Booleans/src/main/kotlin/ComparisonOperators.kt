877c668933d775d075eba84e1e1396c4f47cecb4
Herbert Andrade <herbertpansini@hotmail.com>
2022-07-18 11:21:31 -0300

    67. Comparison Operators

---
+++ Operators_And_Booleans/src/main/kotlin/ComparisonOperators.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val a = 5
+    val b = 1
+
+    println("$a > $b wil return ${a > b}")
+
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println(a == b)
+
+    val result = a != b
+
+    println(result)
+}
